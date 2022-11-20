6764fcba340948c4bd3f0c26154b4b54dec91476
OscStal <oscsta@student.chalmers.se>
2022-11-20 17:10:23 +0100

    some more stuff

--- disease_model/Agent.py
+++ disease_model/Agent.py
@@ -1,12 +1,19 @@
+import random
+
 class Agent():
 
-    def __init__(self, position=(0, 0)) -> None:
+    def __init__(self, pos=(0, 0), rad=1) -> None:
         self.parameters = {}    # If we want different paramters per agent?
-        self.position = position
+        self.pos = pos
+        self.radius = rad
 
         self.S = True
         self.E = False
         self.I = False
         self.R = False
-        
+
+    def random_move(self, pos_limit):
+        new_x = (self.pos[0] + random.random())%pos_limit
+        new_y = (self.pos[1] + random.random())%pos_limit
+        self.pos = (new_x, new_y)
 
--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 # Imports
 import random
 from disease_model.Agent import Agent
+import numpy as np
+from matplotlib import pyplot
 
 
 
 def main():
-    al = []
-    for i in range(10):
-        al.append(Agent(position=(random.random(), random.random())))
-    for agent in al:
-        print(agent.position)
+    ENVIRONMENT_COUNT = 1
+    AGENT_COUNT = 100
+    TIMESTEPS = 1000
+
+    env_size = 100
+    environment = np.zeros((ENVIRONMENT_COUNT, AGENT_COUNT), dtype=object)
+    for i in range(AGENT_COUNT):
+        environment[0, i] = Agent(pos=(env_size*random.random(), env_size*random.random()))
+
+    for t in range(TIMESTEPS):
+        for agent in environment[0]:
+            agent.random_move(env_size)
+            pyplot.plot(agent.pos[0], agent.pos[1], "o")
+        pyplot.show()
 
 if __name__ == "__main__": 
     main()
