5edac6fa5d9a6afd635801d121d53c44a95bd482
OscStal <oscsta@student.chalmers.se>
2022-11-21 20:32:29 +0100

    added some super basic functions

--- disease_model/Agent.py
+++ disease_model/Agent.py
@@ -2,18 +2,21 @@ import random
 
 class Agent():
 
-    def __init__(self, pos=(0, 0), rad=1) -> None:
+    def __init__(self, pos=(0, 0), rad=1, i_rate=0.9, r_rate=0.05) -> None:
+
         self.parameters = {}    # If we want different paramters per agent?
         self.pos = pos
         self.radius = rad
+        self.infect_rate = i_rate
+        self.recover_rate = r_rate
 
         self.S = True
         self.E = False
         self.I = False
         self.R = False
 
-    def random_move(self, pos_limit):
-        new_x = (self.pos[0] + random.random())%pos_limit
-        new_y = (self.pos[1] + random.random())%pos_limit
+    def random_move(self, pos_limit, step_size):
+        new_x = (self.pos[0] + (step_size*random.random()))%pos_limit
+        new_y = (self.pos[1] + (step_size*random.random()))%pos_limit
         self.pos = (new_x, new_y)
 
--- main.py
+++ main.py
@@ -6,6 +6,40 @@ from matplotlib import pyplot
 
 
 
+# Idk if we wanna do it like this
+def timestep() -> None:
+    # Position update for every agent
+    # Infect people
+    # Recover people
+    pass
+
+def infect_one_env(environment: list):
+    for agent in environment:
+        if (agent.S or agent.E) and (random.random() < agent.infect_rate):
+            infect_nearby_agents(environment, agent.pos, agent.radius)
+
+def infect_nearby_agents(env, infection_pos, infection_radius):
+    for agent in env:
+        if agent.S:
+            offset = tuple(np.subtract(infection_pos, agent.pos))
+            distance = np.sqrt(offset[0]*offset[0] + offset[1]*offset[1])
+            if distance < infection_radius:
+                agent.E = True
+                agent.S = False
+                # Set something to track when to go form exposed to infected
+
+def recover_one_env(environment: list) -> None:
+    for agent in environment:
+        if agent.I and (random.random() < agent.recover_rate):
+            agent.I = False
+            agent.R = True
+
+def update_agent_positions(env, step_size, pos_limit) -> None:
+    for agent in env:
+        agent.random_move(pos_limit, step_size)
+
+
+
 def main():
     ENVIRONMENT_COUNT = 1
     AGENT_COUNT = 100
@@ -13,11 +47,16 @@ def main():
 
     env_size = 100
     environment = np.zeros((ENVIRONMENT_COUNT, AGENT_COUNT), dtype=object)
-    for i in range(AGENT_COUNT):
-        environment[0, i] = Agent(pos=(env_size*random.random(), env_size*random.random()))
+    environment = []
+    for _ in range(AGENT_COUNT):
+        environment.append(Agent(pos=(env_size*random.random(), env_size*random.random())))
+
+
+
+
 
     for t in range(TIMESTEPS):
-        for agent in environment[0]:
+        for agent in environment:
             agent.random_move(env_size)
             pyplot.plot(agent.pos[0], agent.pos[1], "o")
         pyplot.show()
